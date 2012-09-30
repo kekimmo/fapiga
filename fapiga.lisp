a8930ec4612b0fe8b525d9cb2ac5626098103ac3
Kimmo Kenttälä <kekimmo@kapsi.fi>
2012-09-30 17:40:56 +0300

    Clean up code and add a game-over state

--- fapiga.lisp
+++ fapiga.lisp
@@ -1,6 +1,10 @@
 
 (ql:quickload "lispbuilder-sdl")
 
+(defpackage :fapiga
+  (:use :cl :sdl))
+
+(in-package :fapiga)
 
 (defparameter *window-width* 800)
 (defparameter *window-height* 600)
@@ -10,11 +14,15 @@
 (defparameter *field-height* 20)
 (defparameter *kill-row* 2)
 (defparameter *field* nil)
-(defparameter *field-x* 0)
-(defparameter *field-y* 0)
+(defparameter *field-x* 3)
+(defparameter *field-y* 2)
+(defparameter *next-x* (+ *field-x*
+                          *field-width*
+                          2
+                          1))
+(defparameter *next-y* *field-y*)
 (defparameter *piece* nil)
 (defparameter *field-top-brick* *field-height*)
-
 
 
 (defstruct form
@@ -224,17 +232,15 @@
 
 
 (defclass piece ()
-  ((x :initform 1
-      :initarg :x
+  ((x :initarg :x
       :reader piece-x)
-   (y :initform 0
-      :initarg :y
+   (y :initarg :y
       :reader piece-y)
-   (form-index :initform 0
-               :initarg :form-index
+   (form-index :initarg :form-index
                :reader piece-form-index)
    (type :initarg :type
          :reader piece-type))
+  (:default-initargs :x 0 :y 0 :form-index 0)
   )
 
 
@@ -256,6 +262,9 @@
 (defun piece-bottom (piece)
   (+ (slot-value piece 'y)
      (form-bottom (piece-current-form piece))))
+
+(defun piece-width (piece)
+  (form-width (piece-current-form piece)))
 
 (defun piece-bounding-edges (piece)
   (with-slots (x y) piece
@@ -292,19 +301,86 @@
                                                 (piece-form-index piece))
                                              (length (piece-type-forms (piece-type piece))))))
 
+(defmacro field-top-brick (field)
+  '*field-top-brick*)
+
 (defmacro field-row (field y)
   `(aref ,field ,y))
 
-(defmacro field-brick-at (field x y)
-  `(aref (field-row ,field ,y) ,x))
+(defun field-brick-at (field x y)
+  (aref (field-row field y) x))
 
-(defun field-put-brick (field x y brick-type)
-  (when (< y *field-top-brick*)
-    (setf *field-top-brick* y))
-  (setf (field-brick-at field x y) brick-type))
+(defun field-set-brick-at (field x y brick-type)
+  (when (< y (field-top-brick field))
+    (setf (field-top-brick field) y))
+  (setf (aref (aref field y) x) brick-type)
+  brick-type)
 
-(defmacro field-top-brick (field)
-  '*field-top-brick*)
+(defsetf field-brick-at field-set-brick-at)
+
+(defparameter *textures* (list))
+(defparameter *textures-colorize-color* (rgb 255 255 0))
+
+(define-condition textures-not-found (error)
+  ((kind :initarg :kind
+         :reader kind))
+  (:report (lambda (c stream) (format stream "Textures not found: ~a." (kind c))))
+  )
+
+(defun get-textures (kind)
+  (or (first (getf *textures* kind))
+      (error 'textures-not-found :kind kind)))
+
+(defun colorize-pixel (pixel-color color)
+  (if (sdl:color= pixel-color *textures-colorize-color*)
+    color
+    pixel-color))
+
+(defun colorize-image (image colorize-color)
+  (let ((new-image (sdl:copy-surface :surface image :fill nil :color-key-fill nil)))
+    (dotimes (x (sdl:width image))
+      (dotimes (y (sdl:height image))
+        (sdl:draw-pixel-* x y :surface new-image
+                          :color (colorize-pixel (sdl:read-pixel-* x y :surface image)
+                                                 colorize-color))))
+    new-image))
+
+(defun load-texture (source)
+  (format t "Loading texture ~a... " source)
+  (prog1
+    (sdl:convert-to-display-format :surface
+                                   (sdl:load-image source :color-key (rgb 255 255 255)))
+    (format t "OK.~%")))
+
+(defun load-and-colorize (source colors)
+  (let ((img (load-texture source)))
+    (format t "Colorizing texture ~a...~%" source)
+    (prog1
+      (loop with textures = (make-hash-table)
+            for color in colors
+            do (setf (gethash color textures) (colorize-image img color))
+            finally (return textures))
+      (format t "OK.~%"))))
+
+(defun load-textures-in (dir)
+  (loop for file in (directory (make-pathname :directory (list :relative dir)
+                                              :name :wild
+                                              :type "bmp"))
+        collect (load-texture file))
+  )
+
+(defun load-textures ()
+  (format t "* Textures *~%")
+  (setf (getf *textures* :brick)
+        (loop for file in (directory (make-pathname :directory '(:relative "images/bricks")
+                                                    :name :wild
+                                                    :type "bmp"))
+              collect (load-and-colorize file *brick-types*)))
+  (setf (getf *textures* :field) (load-textures-in "images/field"))
+  (setf (getf *textures* :next) (load-textures-in "images/next"))
+  (setf (getf *textures* :game-over) (load-textures-in "images/game-over"))
+  (format t "* /Textures *~%")
+  )
 
 
 (defun transform-field-coords (x y)
@@ -317,7 +393,7 @@
   (multiple-value-bind (screen-x screen-y) (transform-field-coords x y)
     ;(sdl:draw-box-* screen-x screen-y *brick-width* *brick-height*
     ;                :color brick-type :stroke-color (rgb 50 50 50))
-    (sdl:draw-surface-at-* (gethash brick-type (first (get-textures :brick)))
+    (sdl:draw-surface-at-* (gethash brick-type (get-textures :brick))
                            screen-x screen-y)
     ))
 
@@ -327,20 +403,41 @@
   ;                :color (rgb 255 255 255))
   ;(sdl:draw-box-* x y (* *field-width* *brick-width*) (* (1+ *kill-row*) *brick-height*)
   ;                :color (rgb 255 200 200))
-  (sdl:draw-surface-at-* (first (get-textures :field)) *field-x* *field-y*)
-  (loop for brick-x from 0 below *field-width*
-        do (loop for brick-y from 0 below *field-height*
-                 for brick = (field-brick-at field brick-x brick-y)
-                 if brick do (draw-brick brick-x brick-y brick)))
+  ;
+  (multiple-value-bind (screen-x screen-y) (transform-field-coords x y)
+    (sdl:draw-surface-at-* (get-textures :field) screen-x screen-y)
+    (loop for brick-x from 0 below *field-width*
+          do (loop for brick-y from 0 below *field-height*
+                   for brick = (field-brick-at field brick-x brick-y)
+                   if brick do (draw-brick (+ 1 x brick-x) (+ 1 y brick-y) brick)))
+
+    )
   )
 
-(defun draw-piece (piece)
-  (with-accessors ((piece-x piece-x) (piece-y piece-y) (brick-type piece-brick-type)) piece
-    (let ((form (piece-current-form piece)))
-      (loop for y from 0 below (form-height form) do
-            (loop for x from 0 below (form-width form)
-                  if (form-brick-at-p form x y)
-                  do (draw-brick (+ piece-x x) (+ piece-y y) brick-type))))))
+(defun draw-piece (piece &key
+                         (offset-x 0)
+                         (offset-y 0)
+                         (x (+ offset-x (piece-x piece)))
+                         (y (+ offset-y (piece-y piece))))
+  (with-accessors ((brick-type piece-brick-type) (form piece-current-form)) piece
+    (loop for brick-y from 0 below (form-height form) do
+          (loop for brick-x from 0 below (form-width form)
+                if (form-brick-at-p form brick-x brick-y)
+                do (draw-brick (+ x brick-x) (+ y brick-y) brick-type)))))
+
+
+(defun draw-next (x y next-piece)
+  (multiple-value-bind (screen-x screen-y) (transform-field-coords x y)
+    (sdl:draw-surface-at-* (get-textures :next) screen-x screen-y))
+  (draw-piece next-piece
+              :x (+ x 1 (truncate (- 6 (piece-width next-piece)) 2))
+              :y (+ y 5)))
+
+
+(defun draw-game-over (x y)
+  (multiple-value-bind (screen-x screen-y) (transform-field-coords x y)
+    (sdl:draw-surface-at-* (get-textures :game-over) screen-x screen-y))
+  )
 
 
 (defun spawn-piece ()
@@ -386,7 +483,7 @@
           do (loop for form-y from top upto bottom
                    for y = (+ piece-y form-y)
                    if (form-brick-at-p form form-x form-y)
-                   do (field-put-brick field x y brick-type)))))
+                   do (setf (field-brick-at field x y) brick-type)))))
 
 
 (defparameter *key-map* '(
@@ -403,16 +500,29 @@
         if (find key keys) return command))
 
 
-(defun command-perform (command)
-  (let ((altered-piece (ecase command
-                         ((:left :right) (piece-move *piece* command))
-                         (:rotate-right (piece-rotate *piece* :right))
-                         (:drop (loop for p = *piece* then (piece-fall p)
-                                      until (landed-p *field* p)
-                                      finally (return p)))
-                         (:fall (piece-fall *piece*)))))
-    (unless (collision-p *field* altered-piece)
-      (setf *piece* altered-piece))))
+(define-condition command-attempted ()
+  ((command :initarg :command
+            :reader command)))
+
+(define-condition command-failed (command-attempted)
+  ((reason :initarg :reason
+           :reader reason)))
+
+
+(defun command-perform (state command)
+  (with-slots (field piece) state
+    (let ((altered-piece (ecase command
+                           ((:left :right) (piece-move piece command))
+                           (:rotate-right (piece-rotate piece :right))
+                           (:drop (loop for p = piece then (piece-fall p)
+                                        until (landed-p field p)
+                                        finally (return p)))
+                           (:fall (piece-fall piece)))))
+      (cond ((collision-p field altered-piece)
+             (signal 'command-failed :command command :reason :no-room))
+            (t
+             (setf piece altered-piece)
+             (signal 'command-attempted :command command))))))
 
 
 
@@ -423,9 +533,16 @@
   (format t "Move row: ~d -> ~d.~%" from to)
   (field-replace-row field to (field-row field from)))
 
+(defun field-make-row ()
+  (make-array *field-width* :initial-element nil))
+
 (defun field-clear-row (field y)
   (format t "Clear row: ~d.~%" y)
-  (field-replace-row field y (make-array *field-width* :initial-element nil)))
+  (field-replace-row field y (field-make-row)))
+
+(define-condition rows-removed ()
+  ((rows :initarg :rows
+         :reader rows)))
 
 (defun remove-full-rows (field &optional (range-from 0) (range-upto (- *field-height* 1)))
   (format t "Check rows ~d-~d.~%" range-from range-upto)
@@ -439,116 +556,115 @@
             if (find y full-rows) do (incf rows-removed)
             else do (field-move-row field y (+ y rows-removed))
             finally (progn (loop for y2 from (+ y 1)
-                          repeat rows-removed
-                          do (field-clear-row field y2))
-                           (incf (field-top-brick field) rows-removed))
-            )
-      0
-      )
+                                 repeat rows-removed
+                                 do (field-clear-row field y2))
+                           (incf (field-top-brick field) rows-removed)
+                           (signal 'rows-removed :rows full-rows))))
+    (length full-rows)
     )
   )
 
 
 (defun make-field ()
   (let ((rows (make-array *field-height*)))
-    (dotimes (y *field-height*)
-      (setf (aref rows y) (make-array *field-width* :initial-element nil)))
-    rows))
+    (map-into rows #'field-make-row)))
 
 
-(defparameter *textures* (list))
-(defparameter *textures-colorize-color* (rgb 255 255 0))
 
-(defun get-textures (kind)
-  (getf *textures* kind))
+(define-condition game-over ()
+  ())
 
-(defun colorize-pixel (pixel-color color)
-  (if (sdl:color= pixel-color *textures-colorize-color*)
-    color
-    pixel-color))
+(defstruct state
+  field
+  (piece nil)
+  (next-piece nil)
+  (fall-counter 0)
+  (points 0)
+  (over nil))
 
-(defun colorize-image (image colorize-color)
-  (let ((new-image (sdl:copy-surface :surface image :fill nil :color-key-fill nil)))
-    (dotimes (x (sdl:width image))
-      (dotimes (y (sdl:height image))
-        (sdl:draw-pixel-* x y :surface new-image
-                          :color (colorize-pixel (sdl:read-pixel-* x y :surface image)
-                                                 colorize-color))))
-    new-image))
+(defparameter *fall-every-nth* 10)
+(defparameter *points-for-row* 100)
 
-(defun load-texture (source)
-  (format t "Loading texture ~a...~%" source)
-  (sdl:convert-to-display-format
-    :surface (sdl:load-image source :color-key (rgb 255 255 255))))
+(defun points-for-removals (row-count)
+  (if (zerop row-count)
+    0
+    (* *points-for-row* row-count row-count)))
 
-(defun load-and-colorize (source colors)
-  (let ((img (load-texture source)))
-    (format t "Colorizing texture ~a...~%" source)
-    (loop with textures = (make-hash-table)
-          for color in colors
-          do (setf (gethash color textures) (colorize-image img color))
-          finally (return textures))))
+(define-condition points-earned ()
+  ((points :initarg :points :reader points)
+   (total :initarg :total :reader total)))
 
-(defun load-textures ()
-  (setf (getf *textures* :brick)
-        (loop for file in (directory (make-pathname :directory '(:relative "images/bricks")
-                                                    :name :wild
-                                                    :type "bmp"))
-              collect (load-and-colorize file *brick-types*)))
-  (setf (getf *textures* :field)
-        (loop for file in (directory (make-pathname :directory '(:relative "images/field")
-                                                    :name :wild
-                                                    :type "bmp"))
-              collect (load-texture file)))
-  )
+(defun add-points (state points)
+  (with-slots ((total points)) state
+    (incf total points)
+    (signal 'points-earned :points points :total total)))
 
+(define-condition game-already-over (error) ())
 
-(define-condition game-over (condition) ())
+(defun state-advance (state &optional command)
+  (with-slots (field piece next-piece fall-counter over) state
+    (when over
+      (error 'game-already-over))
+    (loop until piece
+          do (shiftf piece next-piece (spawn-piece)))
+    (when command
+      (command-perform state command))
+    (let ((landed-p (landed-p field piece)))
+      (when landed-p
+        (lock field piece)
+        (let* ((row-count (remove-full-rows field (piece-top piece) (piece-bottom piece)))
+               (row-points (points-for-removals row-count)))
+          (when (plusp row-points)
+            (add-points state row-points)))
+        (setf piece nil))
+      (incf fall-counter)
+      (when (= fall-counter *fall-every-nth*)
+        (setf fall-counter 0)
+        (unless landed-p
+          (setf piece (piece-fall piece)))))
+    (when (<= (field-top-brick field) *kill-row*)
+      (setf over t)
+      (signal 'game-over))
+    ))
+
 
 (defun sdl-main ()
   (sdl:window *window-width* *window-height* :double-buffer t)
   (sdl:enable-key-repeat nil nil)
   (setf (sdl:frame-rate) 10)
-  (sdl:clear-display sdl:*black*)
   (load-textures)
-  (let ((*field* (make-field))
-        (counter 0)
-        (keys-pressed (list))
-        )
+  (let* ((*field* (make-field))
+         (keys-pressed (list))
+         (state (make-state :field *field*))
+         )
     (sdl:with-events ()
       (:idle
-        (when (not *piece*)
-          (setf *piece* (spawn-piece)))
-
         (let ((command (command-get keys-pressed)))
-          (when (and *piece* command)
-            (command-perform command)
-            ))
+          (unless (state-over state)
+            (handler-bind
+              ((game-over (lambda (c)
+                            (format t "Game over!~%")))
+               (points-earned (lambda (c)
+                                (format t "Points: ~d (+~d)~%" (total c) (points c))))
+               ;(condition (lambda (c)
+               ;             (print c)))
+               )
+
+              (state-advance state (command-get keys-pressed)))))
         (setf keys-pressed (list))
 
-        (let ((landed-p (landed-p *field* *piece*)))
-          (when landed-p
-            (lock *field* *piece*)
-            (remove-full-rows *field* (piece-top *piece*) (piece-bottom *piece*))
-            (setf *piece* nil))
-          (incf counter)
-          (when (= counter 10)
-            (setf counter 0)
-            (unless landed-p
-              (setf *piece* (piece-fall *piece*)))
-            )
+        (sdl:clear-display (rgb 255 255 255))
+        (with-slots (field piece next-piece over) state
+          (draw-field *field-x* *field-y* field)
+          (draw-next *next-x* *next-y* next-piece)
+          (when piece
+            (draw-piece piece :offset-x (1+ *field-x*) :offset-y (1+ *field-y*)))
+          (when over
+            (draw-game-over *field-x* *field-y*))
           )
-
-        (sdl:clear-display sdl:*black*)
-
-        (draw-field *field-x* *field-y* *field*)
-        (when *piece*
-          (draw-piece *piece*))
         (sdl:update-display)
-        (when (<= (field-top-brick *field*) *kill-row*)
-          (sdl:push-quit-event)
-          ;(error 'game-over)
-          )
+        (
+         )
         )
       (:key-down-event (:key key)
        (pushnew key keys-pressed))
